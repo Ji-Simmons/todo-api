2d1be3992e66e04063291d9c472692c1a1d263c8
Ji Simmons <ji.simmons@yahoo.com>
2020-07-05 15:23:35 -0600

    again

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const DBNAME = "blogdb";
 module.exports= {
- PORT: 5555,
+ PORT: process.env.PORT,
  DBNAME,
  APPNAME: 'blog',
  DBURI: `mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@blog-z710y.mongodb.net/${DBNAME}?retryWrites=true&w=majority`
