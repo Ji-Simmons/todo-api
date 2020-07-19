8c259ca352fe41c54d7c49ffa3990afa13bc0af9
Ji Simmons <ji.simmons@yahoo.com>
2020-07-18 21:45:01 -0600

    installed node

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const DBNAME = "tasksdb";
 module.exports= {
- PORT: process.env.PORT,
+ PORT: process.env.PORT || 5555,
  DBNAME,
  APPNAME: 'todo',
  DBURI: `mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@blog-z710y.mongodb.net/${DBNAME}?retryWrites=true&w=majority`
