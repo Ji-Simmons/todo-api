2aab8c674aace2d876d58825a76993e8603eb83c
Ji Simmons <ji.simmons@yahoo.com>
2020-07-19 17:00:42 -0600

    eventlistener?

--- api/tasks/tasks.model.js
+++ api/tasks/tasks.model.js
@@ -13,7 +13,30 @@ const tasksSchema = new mongoose.Schema({
         required: false,
         ref: "date",
     },
-    
+    owner: {
+        type: String,
+        unique: false,
+        required: false,
+        ref: "owner",
+    },
+    description: {
+        type: String,
+        unique: false,
+        required: false,
+        ref: "description",
+    },
+    name: {
+        type: String,
+        unique: false,
+        required: false,
+        ref: "name",
+    },
+    age: {
+        type: String,
+        unique: false,
+        required: false,
+        ref: "age",
+    },
 },
 // If you want your model to store
 // timestamps when things are updated/created
