a9dc167bae14bdbaacb1744cf7ce0c18a2a65617
Ji Simmons <ji.simmons@yahoo.com>
2020-07-18 22:33:17 -0600

    deleted a file

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
-const DBNAME = "tasksdb";
+const DBNAME = "tododb";
 module.exports= {
  PORT: process.env.PORT || 5555,
  DBNAME,
  APPNAME: 'todo',
  DBURI: `mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@blog-z710y.mongodb.net/${DBNAME}?retryWrites=true&w=majority`
- 
  
 }
